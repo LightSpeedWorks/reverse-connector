e01e7da8454b9a79b680733180bb5dff8ec14072
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-02-12 06:21:26 +0900

    forwarder

---
+++ forwarder/forwarder.cmd
@@ -0,0 +1,5 @@
+@node -p '\x1b[44m' & cls
+@pushd %~dp0
+@node forwarder
+@popd
+@pause
---
+++ forwarder/forwarder.js
@@ -0,0 +1,70 @@
+// forwarder.js
+
+void function () {
+	var assert = require('assert');
+	var path = require('path');
+	var net = require('net');
+	var log = require('log-manager').getLogger();
+	var startStatistics = require('../lib/start-statistics');
+
+	log.info('node', process.version, path.basename(__filename));
+	process.title = path.basename(__filename);
+	var configs = require('./forwarder-config.json');
+	log.setLevel(configs.logLevel);
+
+	var forwarderId = 20000;
+	var myName = '(forwarder)';
+	var countUp = startStatistics(log, myName).countUp;
+
+	configs.forwarders.forEach(function (config) {
+		assert(Number(config.forwarderPort), 'config.forwarderPort');
+		assert(Number(config.servicePort),   'config.servicePort');
+
+		log.info(config);
+
+		var serviceNetSvr = net.createServer(
+				{allowHalfOpen:true},
+				function connectionService(c) {
+			log.debug(myName, 'connected.');
+			var s = net.connect(
+					{port:configs.forwarderPort, host:configs.forwarderHost, allowHalfOpen:true},
+					function connectionForwarder() {
+				s.pipe(c);
+				c.pipe(s);
+			});
+			s.on('error', function error(err) {
+				log.warn(myName, 'forwarder error', err);
+				s.destroy();
+			});
+			s.on('end', function end() {
+				log.debug(myName, 'forwarder disconnected');
+			});
+
+			var received = false;
+			c.on('error', function error(err) {
+				log.warn(myName, 'client error', err);
+				c.destroy();
+			});
+			c.on('end', function end() {
+				log.debug(myName, 'client disconnected');
+				if (!received) {
+					log.warn(myName, 'client has gone!');
+				}
+			});
+			//c.on('readable', function readable() {
+			//	var buff = c.read();
+			//	if (!buff) return;
+
+			//	if (!s) {
+			//		s.write(buff);
+			//	}
+			//	received = true;
+			//	c.removeListener('readable', readable);
+			//});
+		}).listen(config.servicePort, function listeningService() {
+			log.info(myName, 'server bound. port', config.servicePort);
+		});
+
+	}); // configs.forEach
+
+}();
